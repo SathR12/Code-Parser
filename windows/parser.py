d27150d70a543ab977b9ad74d0e48205e68229d3
Sathya <74515743+SathR12@users.noreply.github.com>
2022-07-20 12:59:59 -0400

    Create parser.py

---
+++ windows/parser.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+import pytesseract
+import pyperclip
+from PIL import Image 
+
+#set pytesseract path
+pytesseract.pytesseract.tesseract_cmd = r'enter your path to tesseract executable here'
+
+class Parser:
+    def __init__(self, img):
+        self.text = pytesseract.image_to_string(img)
+    
+    def __str__(self):
+        return self.text
+    
+    
+
+        
+        
+    
+    
+        
+        
+        
+        
+        
+    
