9b74969611e57d957f79c50258ef93e7c1bc9cf2
Sathya <74515743+SathR12@users.noreply.github.com>
2022-07-21 20:10:24 +0530

    Create main.py

---
+++ mac/main.py
@@ -0,0 +1,11 @@
+from parser import *
+from snippet import *
+from tkinter import *
+
+
+#run application
+if __name__ == "__main__":
+    root = Tk()
+    SNIPPING = Snipping(root)
+    root.mainloop()
+    
